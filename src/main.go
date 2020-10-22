38c7bf6edf6f2dda27f096e5f3757954cc15aef9
_ 0ne <kouguchi3456@yahoo.co.jp>
2020-10-22 22:22:00 +0900

    modified: server listening log

--- src/main.go
+++ src/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,9 @@ import (
 func main() {
 	app := gin.Default()
 	if app != nil {
-		fmt.Println("Server start ...OK")
+		fmt.Println("Server listening ...OK")
 		app.Run(":8080")
+	} else {
+		log.Panic("Server listening ...NO")
 	}
 }
